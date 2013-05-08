756efe3bd4e62e11623dfcfeba8d6f4e7ac7116c
Luis Perichon <luisperichon@gmail.com>
2013-05-07 22:50:27 -0300

    accounts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130507192212) do
+ActiveRecord::Schema.define(version: 20130508005510) do
+
+  create_table "accounts", force: true do |t|
+    t.string "name"
+  end
 
   create_table "users", force: true do |t|
     t.string   "username"
