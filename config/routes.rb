12e819e8def1b0f0923f27257f7b82b01a4e4e21
Luis Perichon <luisperichon@gmail.com>
2013-05-09 14:41:29 -0300

    changed login/logout routes to the new format

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Assistance::Application.routes.draw do
-  devise_for :users do
+  devise_for :users 
+  devise_scope :user do
     get "/login", :to => "devise/cas_sessions#new"
     post '/logout', to: "devise/cas_sessions#destroy"
   end
