26105ae811ea9f85c7d4e5ad8ee99ba00d6b516d
Luis Perichon <luisperichon@gmail.com>
2013-05-25 14:25:02 -0300

    load padma api keys from local yml on development

--- config/initializers/padma_api.rb
+++ config/initializers/padma_api.rb
@@ -1,9 +1,11 @@
 HYDRA = Typhoeus::Hydra.new
 
+CONFIG = YAML.load_file("#{Rails.root}/config/padma_api.yml")
+
 module Accounts
-  API_KEY = ENV['accounts_key']
+  API_KEY = ENV['accounts_key'] || CONFIG['accounts_key']
 end
 
 module Contacts
-  API_KEY = ENV['contacts_key3']
+  API_KEY = ENV['contacts_key3'] || CONFIG['contacts_key']
 end
