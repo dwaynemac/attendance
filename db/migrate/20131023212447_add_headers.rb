587a88bed656daf8655718ca72d5f06971d31350
Dwayne Macgowan <dwaynemac@gmail.com>
2013-10-29 12:58:09 -0300

    smaller headers field

--- db/migrate/20131023212447_add_headers.rb
+++ db/migrate/20131023212447_add_headers.rb
@@ -1,5 +1,5 @@
 class AddHeaders < ActiveRecord::Migration
   def change
-    add_column :imports, :headers, :text, limit: 4294967295 # LONGTEXT (max 4Gb)
+    add_column :imports, :headers, :text
   end
 end
