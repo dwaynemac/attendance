4153154c7e04da5de0ffa859e150f5db199e157c
Luis Perichon <luisperichon@gmail.com>
2014-02-05 03:36:11 -0300

    we dont need to update unnecesary contacts

--- app/models/padma_contacts_synchronizer.rb
+++ app/models/padma_contacts_synchronizer.rb
@@ -11,10 +11,12 @@ class PadmaContactsSynchronizer
     padma_contacts = PadmaContact.search(select: [:first_name, :last_name, :status, :global_teacher_username], ids: padma_contact_ids, :where => {:updated_at =>  @account.synchronized_at}, per_page: padma_contact_ids.size, username: @account.try(:padma).try(:admin).try(:username), account_name: @account.name)
 
     padma_contacts.each do |padma_contact|
-        contact = contacts.detect {|c| c.padma_id == padma_contact._id} 
-        contact.update_attributes(
+        contact = contacts.detect {|c| c.padma_id == padma_contact._id}
+        if contact
+          contact.update_attributes(
 	        :name => "#{padma_contact.first_name} #{padma_contact.last_name}".strip,
 	        :padma_status => padma_contact.status) 
+      	end
     end
     @account.update_attribute(:synchronized_at, DateTime.now)
   end
