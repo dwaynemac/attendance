84afc0d783ef05d5eaacb4158144d9904205908a
Dwayne Macgowan <dwaynemac@gmail.com>
2014-02-25 19:37:06 -0300

    [#66464436] show minutes alwas as double digit

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,7 +1,18 @@
 module AttendancesHelper
 
   def timeslot_description(timeslot)
-    "'#{timeslot.name}' #{timeslot.start_at.hour}:#{timeslot.start_at.min}-#{timeslot.end_at.hour}:#{timeslot.end_at.min} (#{timeslot.padma_uid})"
+    "'#{timeslot.name}' #{timeslot.start_at.hour}:#{minutes(timeslot.start_at)}-#{timeslot.end_at.hour}:#{minutes(timeslot.end_at)} (#{timeslot.padma_uid})"
+  end
+
+  private
+
+  def minutes(datetime)
+    m = datetime.min
+    if m < 10
+      "0#{m}"
+    else
+      m
+    end
   end
 
 end
