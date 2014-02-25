744d2ba7793e0d8e79c13e9d2a0ef287f01b325d
Dwayne Macgowan <dwaynemac@gmail.com>
2014-02-25 19:29:04 -0300

    [#66464436] show time and teacher of timeslot

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,7 @@
 module AttendancesHelper
+
+  def timeslot_description(timeslot)
+    "'#{timeslot.name}' #{timeslot.start_at.hour}:#{timeslot.start_at.min}-#{timeslot.end_at.hour}:#{timeslot.end_at.min} (#{timeslot.padma_uid})"
+  end
+
 end
