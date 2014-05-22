37e42d322e2286903b7a0ad652a108e9786041c1
Luis Perichon <luisperichon@gmail.com>
2014-05-22 15:40:12 -0300

    [#70592222] Set last_seen_at attribute on contacts when creating attendance

--- app/models/attendance_contact.rb
+++ app/models/attendance_contact.rb
@@ -6,4 +6,13 @@ class AttendanceContact < ActiveRecord::Base
 
   validates :contact, presence: true
   validates :attendance, presence: true
+
+  after_create :set_last_seen_at_on_contacts
+
+  def set_last_seen_at_on_contacts
+  	last_seen_at = DateTime.new(attendance.attendance_on.year, attendance.attendance_on.month, attendance.attendance_on.day, attendance.time_slot.start_at.hour, attendance.time_slot.start_at.min)
+  	unless last_seen_at < contact.padma_contact.last_seen_at
+    	contact.padma_contact.update({:contact => {:last_seen_at => last_seen_at}, :username => attendance.time_slot.padma_uid, :account_name => attendance.account.name})
+    end
+  end
 end
