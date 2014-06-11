7106195e961b0840a0efc8044a3d382816b88895
Dwayne Macgowan <dwaynemac@gmail.com>
2014-06-11 10:37:30 -0300

    [#73046884] spec that identifies error
    - also added git-pivotal-integration gem

--- spec/models/attendance_contact_spec.rb
+++ spec/models/attendance_contact_spec.rb
@@ -12,4 +12,26 @@ describe AttendanceContact do
 	it "should require contact" do
 	  build(:attendance_contact, :contact => nil).should_not be_valid
 	end
+
+  describe "if connection contact-ws fails" do
+    before do
+      PadmaContact.stub(:find).and_return nil
+    end
+
+    it "saves attendances anyway" do
+      ac = build(:attendance_contact)
+      ac.save.should be_true
+    end
+
+  end
+
+  describe "if connection to contacts-ws works" do
+    before do
+      PadmaContact.stub(:find).and_return PadmaContact.new
+    end
+    it "saves attendances anyway" do
+      ac = build(:attendance_contact)
+      ac.save.should be_true
+    end
+  end
 end
