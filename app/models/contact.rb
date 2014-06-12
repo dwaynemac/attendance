d5cbf808eef5db1e7d87ed4d5491ca122a891a2d
Dwayne Macgowan <dwaynemac@gmail.com>
2014-06-12 08:22:37 -0300

    load contact with less data for better perf.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -17,7 +17,9 @@ class Contact < ActiveRecord::Base
 
 	def padma_contact(options={})
     if @padma_contact.nil?
-      @padma_contact = PadmaContact.find(self.padma_id, {select: [:first_name, :last_name, :email, :last_seen_at], account_name: self.account.name})
+      @padma_contact = PadmaContact.find(self.padma_id, {select: [:first_name, :last_name, :email, :last_seen_at],
+                                                         except: [:except_linked, :except_last_local_status],
+                                                         account_name: self.account.name})
     end
     @padma_contact
 	end
