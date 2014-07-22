d87ad8215af16b66edf334795145f06cad53c9df
Dwayne Macgowan <dwaynemac@gmail.com>
2014-07-22 17:20:15 -0300

    use bootstrap pills, set current as active

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -7,4 +7,10 @@ module StatsHelper
       "#{l(@stats.start_on, format: :default)} - #{l(@stats.end_on, format: :default)}"
     end
   end
+
+  def active_pill( period )
+    if (period.nil? && params[:easy_period].nil?) || (period == params[:easy_period])
+      "class=active"
+    end
+  end
 end
