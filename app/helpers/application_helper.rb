ee50abb15295acdd0c18e011fdfaa4b392be8e25
Dwayne Macgowan <dwaynemac@gmail.com>
2014-07-25 11:48:04 -0300

    [#70596828] add feedback button to attendances

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def generate_secure_doorbell_signature()
+      jsonp_secret = 'hosV7LyVnX8r2IOHXUGmSFqvBWuyYXilLgg94aN17pfqeQ50ob5kCcuUHguUaN4H'
+      timestamp = Time.now.to_i
+      token = SecureRandom.hex(50)
+
+      signature = OpenSSL::HMAC.hexdigest(
+                  OpenSSL::Digest::Digest.new('sha256'),
+                  jsonp_secret,
+                  '%s%s' % [timestamp, token])
+
+      return "timestamp: #{timestamp}, token: '#{token}', signature: '#{signature}', ".html_safe
+  end
 end
