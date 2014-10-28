51815e1ff9b3574cd3e614bd233700d0b804f2c4
Dwayne Macgowan <dwaynemac@gmail.com>
2014-10-27 21:28:01 -0300

    bugfix for activerecord session on rails 4.0.0

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
 Assistance::Application.config.session_store :active_record_store
+
+# needed for activerecord-session_store in rails 4.0.0
+ActiveRecord::SessionStore::Session.attr_accessible :data, :session_id
