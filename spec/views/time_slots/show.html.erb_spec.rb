a2e75bcb7de8211fa6f9654dac6ce2899f119475
Dwayne Macgowan <dwaynemac@gmail.com>
2015-03-06 17:48:45 -0300

    [#89825546] fix view spec

--- spec/views/time_slots/show.html.erb_spec.rb
+++ spec/views/time_slots/show.html.erb_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "time_slots/show" do
   before(:each) do
     @time_slot = create(:time_slot)
+    @students = @time_slot.contacts.students
   end
 
   it "renders attributes in <p>" do
