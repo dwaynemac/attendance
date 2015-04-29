bf1c2ec9dda9ea4484caa438593ff6b32475cea4
Luis Perichon <luisperichon@gmail.com>
2015-04-28 23:45:35 -0300

    use Contact.get_by_padma_id on Attendance creation

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -21,13 +21,10 @@ class Attendance < ActiveRecord::Base
   end
 
   def padma_contacts= padma_contacts
-  	contact_ids = []
-  	padma_contacts.each do |padma_contact_id|
-    	unless contact = Contact.find_by_padma_id(padma_contact_id)
-        padma_contact = PadmaContact.find(padma_contact_id, select: [:first_name, :last_name, :local_status], :username => time_slot.padma_uid,  :account_name => time_slot.account.name)
-        contact = Contact.create(padma_id: padma_contact_id, account_id: time_slot.account.id, name: "#{padma_contact.first_name} #{padma_contact.last_name}", padma_status: padma_contact.local_status)
-      end
-    	contact_ids << contact.id
+    contact_ids = []
+    padma_contacts.each do |padma_contact_id|
+      contact = Contact.get_by_padma_id(padma_contact_id, time_slot.account_id)
+      contact_ids << contact.id
     end
     self.contact_ids = contact_ids
   end
