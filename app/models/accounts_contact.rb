b047c1f674df1d1b3fa75534b7ece6e8b905b0ef
Dwayne Macgowan <dwaynemac@gmail.com>
2015-05-12 16:32:17 -0300

    [#94228974] only one link account-contact

--- app/models/accounts_contact.rb
+++ app/models/accounts_contact.rb
@@ -5,5 +5,7 @@ class AccountsContact < ActiveRecord::Base
   validates :account, presence: true
   validates :contact, presence: true
 
+  validates_uniqueness_of :contact_id, scope: [:account_id]
+
   attr_accessible :account_id, :contact_id, :padma_status
 end
--- spec/models/accounts_contact_spec.rb
+++ spec/models/accounts_contact_spec.rb
@@ -4,5 +4,11 @@ describe AccountsContact do
   it { should belong_to(:contact) }
 	
   it { should belong_to(:account) }
+
+  it "validates uniqueness of [account,contact]" do
+    ac = create(:accounts_contact)
+    inv = build(:accounts_contact, account: ac.account, contact: ac.contact)
+    expect(inv).to be_invalid
+  end
   
 end
