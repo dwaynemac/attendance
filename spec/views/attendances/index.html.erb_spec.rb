cd369cc415173f9603aebe9331f3976a3d546592
Luis Perichon <luisperichon@gmail.com>
2015-05-18 14:55:36 -0300

    fix spec

--- spec/views/attendances/index.html.erb_spec.rb
+++ spec/views/attendances/index.html.erb_spec.rb
@@ -17,6 +17,15 @@ describe "attendances/index" do
     assign(:time_slots_wout_day, [])
 
     assign(:view_range,(1..7))
+    @user = build(:user)
+    User.any_instance.stub(:padma_user).and_return(PadmaUser.new(:username => @user.username, :email => "test@test.com"))
+    Account.any_instance.stub(:padma).and_return(PadmaAccount.new(:name => @user.current_account.name))
+    User.any_instance.stub(:enabled_accounts).and_return([PadmaAccount.new(:name => @user.current_account.name)])
+    Account.any_instance.stub(:usernames).and_return(["username"])
+    @user.save
+    
+	view.stub(:current_user) { @user }
+
   end
 
   it "renders a list of attendances" do
