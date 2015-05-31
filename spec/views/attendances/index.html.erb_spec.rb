3a868551b4a25b2615b1dba6e448346ca2398195
Alex Falke <afalkear@gmail.com>
2015-05-31 15:16:58 -0300

    [#94579524] assign variables to spec

--- spec/views/attendances/index.html.erb_spec.rb
+++ spec/views/attendances/index.html.erb_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe "attendances/index" do
   before(:each) do
     account = create(:account)
-    
     assign(:attendances, [
       create(:attendance, :account => account),
       create(:attendance, :account => account)
@@ -17,6 +16,9 @@ describe "attendances/index" do
     assign(:time_slots_wout_day, [])
 
     assign(:view_range,(1..7))
+    assign(:days_from, 0)
+    assign(:days_to, 7)
+    assign(:day_span, 7)
     @user = build(:user)
     User.any_instance.stub(:padma_user).and_return(PadmaUser.new(:username => @user.username, :email => "test@test.com"))
     Account.any_instance.stub(:padma).and_return(PadmaAccount.new(:name => @user.current_account.name))
