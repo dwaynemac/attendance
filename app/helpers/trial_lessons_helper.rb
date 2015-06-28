9261e0943a05ebd0e68b965049d5ade8e334a3b0
Dwayne Macgowan <dwaynemac@gmail.com>
2015-06-28 20:48:14 -0300

    [#97293284]add username

--- app/helpers/trial_lessons_helper.rb
+++ app/helpers/trial_lessons_helper.rb
@@ -4,6 +4,7 @@ module TrialLessonsHelper
       {
         id: t.id,
         account_name: t.account.name,
+        username: t.padma_uid,
         contact_id: t.contact.padma_id,
         trial_on: t.trial_on,
         assisted: t.assisted,
