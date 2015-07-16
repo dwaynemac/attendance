c4a75eb622f5aa839bdffb2f50bc7fe530622a92
Dwayne Macgowan <dwaynemac@gmail.com>
2015-07-16 19:32:51 -0300

    [#98911180]add attribute and forwardit in broadcast

--- app/models/trial_lesson.rb
+++ app/models/trial_lesson.rb
@@ -21,6 +21,8 @@ class TrialLesson < ActiveRecord::Base
   attr_accessor :skip_broadcast
   after_create :broadcast_create, unless: :skip_broadcast
 
+  attr_accessor :avoid_mailing_triggers
+
   after_destroy :destroy_activity
 
   ##
@@ -106,10 +108,11 @@ class TrialLesson < ActiveRecord::Base
   def as_json_for_messaging
     json = as_json
     json["trial_at"] = trial_at
-    json["contact_id"] = contact.padma_id
+    json["contact_id"] = padma_contact_id
     json["recipient_email"] = contact.padma_contact(account).email
     json["username"] = padma_uid
     json["account_name"] = account.name
+    json["avoid_mailing_triggers"] = avoid_mailing_triggers
     json
   end  
 
