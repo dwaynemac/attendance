8a63756408e1cc47c791fbb162b2a55fd2c240ed
Dwayne Macgowan <dwaynemac@gmail.com>
2015-09-07 12:46:55 -0300

    retry 3 times, and avoid nil

--- app/models/padma_contacts_synchronizer.rb
+++ app/models/padma_contacts_synchronizer.rb
@@ -10,27 +10,33 @@ class PadmaContactsSynchronizer
 
     since = (@account.synchronized_at || Date.today) - wayback
 
-    # Get all contacts updated since last sync.
-    padma_contacts = PadmaContact.search(select: [:first_name,
-                                                  :last_name,
-                                                  :local_status,
-                                                  :local_statuses,
-                                                  :global_teacher_username],
-                                         where: {
-                                           # local_status: [:student, :former_student], -- BUG - currently not working if used with together with updated_at filter
-                                           updated_at: since.to_date
-                                         },
-                                         account_name: @account.name)
-
-    # Iterate over them
-    padma_contacts.each do |padma_contact|
-      next unless padma_contact.local_status.try(:to_sym).in?([:student,:former_student]) # -- BUG -- filter here until local_status bug solved
-      contact = Contact.find_or_create_by padma_id: padma_contact.id
-
-      if contact
-        contact.sync_from_contacts_ws(padma_contact)
-      end
+    attempts = 0
+    while padma_contacts.nil? && attempts <= 3 do
+      # Get all contacts updated since last sync.
+      padma_contacts = PadmaContact.search(select: [:first_name,
+                                                    :last_name,
+                                                    :local_status,
+                                                    :local_statuses,
+                                                    :global_teacher_username],
+                                           where: {
+                                             # local_status: [:student, :former_student], -- BUG - currently not working if used with together with updated_at filter
+                                             updated_at: since.to_date
+                                           },
+                                           account_name: @account.name)
+      attempts += 1
     end
-    @account.update_attribute(:synchronized_at, DateTime.now)
+
+    if padma_contacts
+      # Iterate over them
+      padma_contacts.each do |padma_contact|
+        next unless padma_contact.local_status.try(:to_sym).in?([:student,:former_student]) # -- BUG -- filter here until local_status bug solved
+        contact = Contact.find_or_create_by padma_id: padma_contact.id
+
+        if contact
+          contact.sync_from_contacts_ws(padma_contact)
+        end
+      end
+      @account.update_attribute(:synchronized_at, DateTime.now)
+    end
   end
 end
