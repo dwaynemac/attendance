b36ae735decb8f6e2b46f0129616b0ed4f094f75
Dwayne Macgowan <dwaynemac@gmail.com>
2015-10-26 13:12:54 -0300

    blur future classes. add current-day class. add pending class

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,5 +1,9 @@
 module AttendancesHelper
 
+  def future_lesson?(date,time_slot)
+	  lesson_time = Time.zone.local(date.year, date.month, date.day, time_slot.start_at.hour, time_slot.start_at.min) >= Time.zone.now
+  end
+
   def keep_params
     params.select{|k,v| k.in?(%W(days_to days_from only_pending username))}
   end
