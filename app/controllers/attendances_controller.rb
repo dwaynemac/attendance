9c72b281bdf1704cacd75f4f79262a63ec5c27b5
Dwayne Macgowan <dwaynemac@gmail.com>
2015-10-27 07:52:06 -0300

    avoid nil

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -90,10 +90,12 @@ class AttendancesController < ApplicationController
     }.last # Get the last one
 
     # If there is an attendance already set for the recent timeslot dont show it
-    if Attendance.where(time_slot_id: recent.time_slot_id, attendance_on: Time.zone.today).exists?
-      nil
-    else
-      recent
+    if recent.present?
+      if Attendance.where(time_slot_id: recent.time_slot_id, attendance_on: Time.zone.today).exists?
+        nil
+      else
+        recent
+      end
     end
   end
   
