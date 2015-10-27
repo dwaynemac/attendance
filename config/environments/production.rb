66f88f2050328c7106671e2eae9da703ba2e7ec8
Dwayne Macgowan <dwaynemac@gmail.com>
2015-10-27 00:15:40 -0300

    disable CDN for test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,7 +58,7 @@ Assistance::Application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-  config.action_controller.asset_host = "d2n08rlx9rtiof.cloudfront.net" # attendance CDN
+  # config.action_controller.asset_host = "d2n08rlx9rtiof.cloudfront.net" # attendance CDN
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
