58964655383f3983a802a51b806a29efde3791f2
Dwayne Macgowan <dwaynemac@gmail.com>
2016-12-03 02:08:28 +0000

    consider cloud9 c9 development environment

--- config/initializers/padma_api.rb
+++ config/initializers/padma_api.rb
@@ -8,15 +8,24 @@ end
 
 module Accounts
   API_KEY = ENV['accounts_key'] || CONFIG['accounts_key']
+  if ENV['C9_USER']
+    HOST = APP_CONFIG['accounts-url'].gsub("http://",'')
+  end
 end
 
 module Contacts
   API_KEY = ENV['contacts_key'] || CONFIG['contacts_key']
+  if ENV['C9_USER']
+    HOST = APP_CONFIG['contacts-url'].gsub("http://",'')
+  end
 end
 
 module ActivityStream
   API_KEY = ENV['activities_key'] || CONFIG['activities_key']
   LOCAL_APP_NAME = 'attendance'
+  if ENV['C9_USER']
+    HOST = APP_CONFIG['activity-stream-url'].gsub("https://",'')
+  end
 end
 
 
