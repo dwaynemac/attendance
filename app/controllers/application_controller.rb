e01dac3901226d34512988b97f63c46be3299c14
Dwayne Macgowan <dwaynemac@gmail.com>
2019-10-09 18:50:30 -0300

    secret login BEFORE sso login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  before_filter :get_sso_session
   before_filter :secret_key_login
+  before_filter :get_sso_session
   
   before_filter :authenticate_user!
   
