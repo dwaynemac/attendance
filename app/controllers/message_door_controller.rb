1d05339b17689508a3e87cde686eee4aec52e651
Dwayne Macgowan <dwaynemac@gmail.com>
2021-12-04 19:56:25 -0300

    catch SNS message 'created_contact'

--- app/controllers/message_door_controller.rb
+++ app/controllers/message_door_controller.rb
@@ -25,7 +25,7 @@ class MessageDoorController < ApplicationController
           case sns_topic
           when "sso_session_destroyed"
             set_sso_session_destroyed_flag(sns_message[:username])
-          when "updated_contact"
+          when "updated_contact", "created_contact"
             if sns_message[:id] && sns_message[:account_name]
               Delayed::Job.enqueue FetchCrmContactJob.new(id: sns_message[:id],
                 account_name: sns_message[:account_name])
