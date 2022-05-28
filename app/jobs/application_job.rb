5065e29bd2ba0e09ce2eac3cfd3dfd74d92ec950
Dwayne Macgowan <dwaynemac@gmail.com>
2022-05-28 19:05:01 -0300

    job.queue retorna siempre el DJ

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -6,16 +6,29 @@ class ApplicationJob
     @attributes = attributes
   end
 
-  def already_queued?
-    Delayed::Job.where("handler = ?", self.to_yaml)
-                .where("attempts < ?", Delayed::Worker.max_attempts)
-                .exists?
+  def perform
+    # child class should implement this method
+    raise NotImplementedError
   end
 
+  # hay jobs iguales a mí pendientes
+  def already_queued?
+    duplicate_jobs.where("attempts < ?", Delayed::Worker.max_attempts).exists?
+  end
+
+  # otros jobs iguales a mí
+  def duplicate_jobs
+    Delayed::Job.where("handler = ?", self.to_yaml)
+  end
+
+  def pending_duplicate_job
+    duplicate_jobs.where("attempts < ?", Delayed::Worker.max_attempts).first
+  end
+
+  # Encola el job. Si YA estaba encolado retorna el job existente
+  # @return [Delayed::Job]
   def queue
-    unless already_queued?
-      Delayed::Job.enqueue self
-    end
+    pending_duplicate_job || Delayed::Job.enqueue(self)
   end
 
 end
