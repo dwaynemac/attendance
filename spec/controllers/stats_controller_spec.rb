a715821680af3843fb6e761288c86648449a7f25
afalkear <afalkear@gmail.com>
2022-08-25 17:41:22 -0300

    minor change to stats controller spec

--- spec/controllers/stats_controller_spec.rb
+++ spec/controllers/stats_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe StatsController do
+  let(:user1){create(:user, username: "123dw.ay_ne@hotmasil.com")}
   before (:each) do
-    allow_any_instance_of(Account).to receive(:usernames).and_return ['dwayne']
+    allow_any_instance_of(Account).to receive(:usernames).and_return [user1.username]
     sign_in_as_a_valid_user
   end
   describe "GET /stats" do
@@ -13,12 +14,12 @@ describe StatsController do
   end
 
   describe "GET /stats?easy_period=last_month" do
-    let(:time_slot){create(:time_slot, :account => @user.current_account, :padma_uid => "dwayne")}
-    let(:attendance){create(:attendance, :account => @user.current_account, :attendance_on => 1.month.ago, :time_slot => time_slot, :username => 'dwayne')}
+    let(:time_slot){create(:time_slot, :account => @user.current_account, :padma_uid => user1.username)}
+    let(:attendance){create(:attendance, :account => @user.current_account, :attendance_on => 1.month.ago, :time_slot => time_slot, :username => user1.username)}
     let(:contact){create(:contact)}
 
     before do
-      allow(CrmLegacyContact).to receive(:find).and_return CrmLegacyContact.new
+      allow(CrmLegacyContact).to receive(:search).and_return [user1]
       contact.accounts_contacts.create!(:account_id => @user.current_account.id, :padma_status => :student)
       attendance.contacts << contact
     end
