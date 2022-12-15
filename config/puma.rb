4b8df3321a6c746c7dde1fb22ab6230233dfcf1d
Dwayne Macgowan <dwaynemac@gmail.com>
2022-12-15 10:14:37 -0300

    add puma.rb

---
+++ config/puma.rb
@@ -0,0 +1,25 @@
+#workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
+min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
+threads min_threads_count, max_threads_count
+
+
+# Use the `preload_app!` method when specifying a `workers` number.
+# This directive tells Puma to first boot the application and load code
+# before forking the application. This takes advantage of Copy On Write
+# process behavior so workers use less memory.
+#
+#preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3020
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+      Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
